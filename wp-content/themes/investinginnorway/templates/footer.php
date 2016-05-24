aa8a2b157d22c311d99b165f60da2e61dcd2deb5
Knut Sorknes <knut.sorknes@makingwaves.com>
2016-05-24 15:52:36 +0200

    adde to top link, footer

--- wp-content/themes/investinginnorway/templates/footer.php
+++ wp-content/themes/investinginnorway/templates/footer.php
@@ -1,5 +1,12 @@
 <footer class="footer" role="contentinfo">
     <div class="container">
-        <p class="text-muted">Copyright &copy; 2016, <?php bloginfo( 'name' ); ?></p>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <p class="text-muted">Copyright &copy; <?php echo date("Y"); ?>, <?php bloginfo( 'name' ); ?></p>
+            </div>
+            <div class="col-xs-12 col-sm-6 to-top">
+                <p><a href="#home" title="To top">To top</a></p>
+            </div>
+        </div>
     </div>
 </footer>
