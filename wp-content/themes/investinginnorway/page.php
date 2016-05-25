ecd8abfff58a02a0a7265d37234c7871e9bd3b0c
Knut Sorknes <knut.sorknes@makingwaves.com>
2016-05-25 13:54:09 +0200

    default page template

--- wp-content/themes/investinginnorway/page.php
+++ wp-content/themes/investinginnorway/page.php
@@ -1,4 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
+    <?php get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+</article>
 <?php endwhile; ?>
